dd9cb0f4644fc57e27721ebe26e4e1efd18239d9
Mac Nisbet <mfnisbet@lakeheadu.ca>
2022-11-29 20:30:11 -0500

    Updated dataMemory
    Updated dataMemory module to standardize inputs

--- 0651-Lab-Project/dataMemory.v
+++ 0651-Lab-Project/dataMemory.v
@@ -1,31 +1,34 @@
 //Data Memory Module
-module dataMemory(clk, address, mem_write, mem_read, write_data, read_data);
+module dataMemory(clk, op, ra, address, read_data);
 
 input clk;
-input mem_write; // Memory write signal
-input mem_read; // Memory read signal
-input [7:0]write_data; // Data written to address
-input [7:0]address; // Second byte address
-output [7:0] read_data; // Output of mem address
- 
+input [7:0]ra; // Data from register being operated on
+input [3:0]op; // Opcode from byte 1
+input [7:0]address; // Address in data memory array
+output [7:0] read_data;
+
 reg [7:0]read_data;
 reg [7:0]datamem[0:255]; // Memory of 256 bytes
 
 integer i;
 initial begin
-	read_data <= 0; // Clear read data
+	read_data <= 0; // Set read data to 0
 	for (i = 0; i < 256; i = i + 1) begin 
 		datamem[i] = i; // Fills data memory with address number
 	end
 end
 
 always @(posedge clk) begin
-	if(mem_write == 1'b1) begin // If mem_write is enabled
-		datamem[address] <= write_data; // Memory address space is set to write_data
-	end
-	if(mem_read == 1'b1) begin // If mem_read is enabled
+	if(op == 4'b1101) begin // Load instruction OP=13
 		read_data <= datamem[address]; // Read_data is set to those values of datamem address
 	end
+	if(op == 4'b1110) begin // Store instruction OP=14
+		datamem[address] <= ra; // Memory address space is set to write_data
+	end
+	if(op == 4'b1111) begin
+		read_data <= address; 
+	end
+	
 end
 
 endmodule 
