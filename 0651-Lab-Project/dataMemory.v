8a58beac4390e53531f97e89c8b6cc9c39353dc7
Mac Nisbet <mfnisbet@lakeheadu.ca>
2022-11-30 20:55:00 -0500

    Update dataMemory.v
    Update data memory to try and fix no-signal issue by changing types, still not functional?

--- 0651-Lab-Project/dataMemory.v
+++ 0651-Lab-Project/dataMemory.v
@@ -1,35 +1,38 @@
 //Data Memory Module
-module dataMemory(clk, op, ra, address, read_data);
+module dataMemory(clk, op, ra_dm, addr_dm, dm_out);
 
 input clk;
-input [7:0]ra; // Data from register being operated on
+input [7:0]ra_dm; // Data from register being operated on
 input [3:0]op; // Opcode from byte 1
-input [7:0]address; // Address in data memory array
-output [7:0] read_data;
+input [7:0]addr_dm; // Address in data memory array
+output [7:0] dm_out;
 
-reg [7:0]read_data;
+reg [7:0] read_data_dm;
 reg [7:0]datamem[0:255]; // Memory of 256 bytes
 
 integer i;
 initial begin
-	read_data <= 0; // Set read data to 0
+read_data_dm <= 0; // Initial value of read_data_dm set to 0
 	for (i = 0; i < 256; i = i + 1) begin 
 		datamem[i] = i; // Fills data memory with address number
 	end
 end
 
+
 always @(posedge clk) begin
 	if(op == 4'b1101) begin // Load instruction OP=13
-		read_data <= datamem[address]; // Read_data is set to those values of datamem address
+		read_data_dm <= datamem[addr_dm]; // Read_data is set to those values of datamem address
 	end
-	if(op == 4'b1110) begin // Store instruction OP=14
-		datamem[address] <= ra; // Memory address space is set to write_data
+	else if(op == 4'b1110) begin // Store instruction OP=14
+		datamem[addr_dm] <= ra_dm; // Memory address space is set to write_data
 	end
-	if(op == 4'b1111) begin
-		read_data <= address; 
+	else if(op == 4'b1111) begin
+		read_data_dm <= addr_dm; 
 	end
 	
 end
+
+assign dm_out = read_data_dm; 
 
 endmodule 
 	
